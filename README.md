f7b8cd63ec502a7f1742fd916a91c189dfbb8a84
Sean Lim <lim_s@outlook.com>
2019-10-18 19:32:16 -0500

    item quantity and server update, more readme

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# mysqlstore
-week 12 hw
+# Welcome!
+
+## Overview
+
+This is a node application designed to emulate an online store. It keeps track of total stock, and allows the user to purchase items as long as the amount requested is available. I like games, so to keep it interesting I made it more like an RPG shop instead stocking it like a normal store.
+
+---
+
+## Requirements
+
+You'll need both the `inquirer` and `mysql` node packages, so make sure to install them.
+
+```javascript
+$ npm i // the included package.json file will make this work
+```
+
+## How it Works
+
+Just like you'd expect, you'll be prompted to enter the store, select a category and an item that you'd like to purchase, and then say how many you want. Since we're not actually paying for anything here(oh no D:), the app won't keep track of how much money you have. Feel free to
+
+![image](./image)
--- customer.js
+++ customer.js
@@ -44,31 +44,138 @@ function afterConnection() {
         }
       ])
       .then(function(enter) {
-        first(enter);
+        return first(enter);
+      })
+      .catch(function(error) {
+        console.log(error);
+        connection.end();
       });
   });
 }
 function first(enter) {
-  if (enter.action === "buy") {
+  if (enter.action != "exit") {
     inquirer
       .prompt([
         {
           message: "what are you looking for?",
           type: "list",
-          choices: ["medicine", "weapon", "clothing", "material"],
+          choices: ["medicine", "weapon", "clothing", "material", "exit"],
           name: "buyType"
         }
       ])
       .then(function(category) {
-        inquirer.prompt([
-          {
-            type: "list",
-            choices: inventory[category.buyType],
-            name: "itemDisplay"
-          }
-        ]);
+        if (category.buyType === "exit") {
+          return goodbye();
+        }
+        return second(category);
+      })
+      .catch(function(error) {
+        console.log(error);
+        connection.end();
       });
   } else {
-    return console.log("come again soon!");
+    return goodbye();
   }
+}
+function second(category) {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        choices: inventory[category.buyType],
+        message: "here's what I have!",
+        name: "itemDisplay"
+      }
+    ])
+    .then(function(pickItem) {
+      inventory[category.buyType].forEach(heldItem => {
+        if (pickItem.itemDisplay == heldItem.name) {
+          third(heldItem);
+        }
+      });
+    })
+    .catch(function(error) {
+      console.log(error);
+      connection.end();
+    });
+}
+function third(heldItem) {
+  inquirer
+    .prompt([
+      {
+        type: "number",
+        message: `${heldItem.name}? those are ${heldItem.price}G, and I have ${heldItem.quantity}. how many do you want?`,
+        name: "buyCount"
+      }
+    ])
+    .then(function(orderAmount) {
+      if (orderAmount.buyCount > heldItem.quantity || orderAmount < 0) {
+        console.log("please ask for an amount I can actually sell you...");
+        third(heldItem);
+      } else if (orderAmount.buyCount === 0) {
+        console.log("that's fine, take your time!");
+        first({ action: "enter" });
+      } else if (
+        orderAmount.buyCount > 0 &&
+        orderAmount.buyCount <= heldItem.quantity
+      ) {
+        const total = orderAmount.buyCount * heldItem.price;
+        const newQuantity = heldItem.quantity - order.buyCount;
+        fourth(total, heldItem.name, newQuantity);
+      } else {
+        console.log("err... what?");
+        third(heldItem);
+      }
+    })
+    .catch(function(error) {
+      console.log(error);
+      connection.end();
+    });
+}
+
+function fourth(confirmAmount, itemName, newQuantity) {
+  console.log(confirmAmount);
+  inquirer
+    .prompt([
+      {
+        type: "confirm",
+        message: `your total will be ${confirmAmount}G. is this okay?`,
+        name: "isConfirm"
+      }
+    ])
+    .then(function(toUpdate) {
+      if (toUpdate.isConfirm) {
+        update(itemName, newQuantity);
+      }
+    })
+    .catch(function(error) {
+      console.log(error);
+      connection.end();
+    });
+}
+
+function update(itemName, newQuantity) {
+  console.log("thanks for your business!");
+  const purchase = connection.query(
+    "UPDATE products SET ? WHERE ?",
+    [
+      {
+        quantity: newQuantity
+      },
+      {
+        name: itemName
+      }
+    ],
+    function(err, res) {
+      if (err) throw err;
+      console.log(res.affectedRows + " products updated!\n");
+      // Call deleteProduct AFTER the UPDATE completes
+      deleteProduct();
+    }
+  );
+}
+
+function goodbye() {
+  connection.end();
+  return console.log("come again soon!");
 }
